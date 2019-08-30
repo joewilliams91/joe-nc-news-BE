4acbc1d6d08019baa256061e1b528c4337ae9661
Joe Williams <joewilliams91@hotmail.co.uk>
2019-08-30 09:57:34 +0100

    readme file created

--- db/migrations/20190823121718_articles.js
+++ db/migrations/20190823121718_articles.js
@@ -2,7 +2,7 @@ exports.up = function(knex) {
   return knex.schema.createTable("articles", articlesTable => {
     articlesTable.increments("article_id").primary();
     articlesTable.string("title").notNullable();
-    articlesTable.text("body").notNullable();
+    articlesTable.string("body", 10000).notNullable();
     articlesTable.integer("votes").defaultTo(0);
     articlesTable
       .string("topic")
